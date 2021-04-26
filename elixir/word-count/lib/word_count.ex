065e3cfa30b30de198a5bdd04f0630e0d464bffd
nau101 <rna0517@gmail.com>
2021-04-26 04:24:34 -0400

    update on regex and functions

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,10 +6,8 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    words = String.replace(sentence,~r/(?=[^\w-\p{L}])[\p{P}\p{S}]/u," ")
-    |> String.replace(~r/[_]/, " ")
-    |> String.downcase()
-    |> String.split(~r{\s} , trim: true)
+    words = String.downcase(sentence)
+    |> String.split(~r/(?=[^w-])[[:punct:]\s]+/u , trim: true) # (?=[^w-]) excludes hyphen(-) from [:punct:]
     Enum.reduce(words, %{}, fn x,acc -> Map.put(acc,x,occurs(words,x)) end)
 
   end
