c246293565a95c6b31d7c537e4b66f42dff8747b
nau101 <rna0517@gmail.com>
2021-06-12 14:44:43 -0400

    Finished Roman numerals exs

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,50 +1,40 @@
 defmodule RomanNumerals do
-  @doc """
+  @moduledoc """
   Convert the number to a roman number.
   """
   @base_n %{
             1 => "I",
+            4 => "IV",
             5 => "V",
+            6 => "VI",
+            9 => "IX",
             10 => "X",
+            40 => "XL",
             50 => "L",
+            90 => "XC",
             100 => "C",
+            400 => "CD",
             500 => "D",
+            900 => "CM",
             1000 => "M"
           }
 
+  @r_keys Map.keys(@base_n) |> Enum.sort(&(&1 > &2)) # extract keys to list from high to low
+
   @spec numeral(pos_integer) :: String.t()
-
   def numeral(number) do
-
-      num_inc = Map.keys(@base_n)
       result = ""
-
-      if Enum.find(num_inc, fn x -> x == number end)  do
-        @base_n[number]
-      else
-       rom_num(number,result)
-      end
-    end
-
-
-  def rom_num(num,res) when num <= 0 do
-    res
+      to_rnumeral(number,@r_keys,result)
   end
 
-  def rom_num(num,_res) when num > 3 do
-    if num < 5 do
-      @base_n[1] <> @base_n[5]
+  defp to_rnumeral(0,_,res), do: res
+  defp to_rnumeral(num,[max|rest] = keys, res) when num >= 0 do
+    if num >= max do
+      to_rnumeral(num-max,keys, res<>@base_n[max])
     else
-
+      to_rnumeral(num, rest, res)
     end
   end
-
-  def rom_num(num,res) do
-    result = res <> @base_n[1]
-    rom_num(num - 1, result)
-  end
-
-
 
 
 end
