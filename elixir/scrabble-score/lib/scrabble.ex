fd3f19bb801104fefa909a878cf1a4138e0719ad
nau101 <rna0517@gmail.com>
2021-06-18 17:39:29 -0400

    Finished Scrabble eercise

--- elixir/scrabble-score/lib/scrabble.ex
+++ elixir/scrabble-score/lib/scrabble.ex
@@ -12,20 +12,40 @@ defmodule Scrabble do
 
   @l_keys Map.keys(@l_values)
 
+
+  @doc """
+   Creating a new map of letter(key) to score(value) map.
+
+    seed_map = %{"A => 1, "B" => 1 ..etc}
+
+  """
+  def seed_map do
+    Enum.reduce(@l_keys,%{}, fn x, acc -> list_to_map(x,acc) end)
+  end
+
+  defp list_to_map(key,acc) do
+    new_map = Map.new(@l_values[key], fn a -> {a,key} end )
+    Map.merge(acc,new_map)
+  end
+
+
   @doc """
   Calculate the scrabble score for the word.
   """
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
-    String.split(word,"",trim: true)
-    |> Enum.reduce(0, fn x,acc -> match(x,acc) end)
-
+    if String.length(word) >= 1 do
+        if String.contains?(word," \t\n") do
+          0
+        else
+          map_l = seed_map()
+          word
+          |> String.upcase()
+          |> String.split("",trim: true)
+          |> Enum.reduce(0, fn x,acc -> acc + Map.fetch!(map_l,x) end)
+        end
+    else
+       0
+    end
   end
-
-  defp match(letter,total) do
-    @l_keys
-    |> Enum.reduce(0,fn x,acc -> )
-  end
-
-
 end
