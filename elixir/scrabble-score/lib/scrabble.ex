8f702bf966969c622dbae82683df06268e267f50
nau101 <rna0517@gmail.com>
2021-06-18 23:07:06 -0400

    Updated   code after seeing community solutions. Reeplsaced seed map wiht comprehension.

--- elixir/scrabble-score/lib/scrabble.ex
+++ elixir/scrabble-score/lib/scrabble.ex
@@ -1,51 +1,31 @@
 defmodule Scrabble do
 
-  @l_values %{
-    1 => ["A","E","I","O","U","L","N", "R", "S", "T"],
-    2 => ["D", "G"],
-    3 => ["B","C","M","P"],
-    4 => ["F","H","V","W","Y"],
-    5 => ["K"],
-    8 =>["J", "X"],
-    10 => ["Q", "Z"]
+  @l_scores %{
+    'AEIOULNRST' => 1,
+    'DG' => 2,
+    'BCMP' => 3,
+    'FHVWY' => 4,
+    'K' => 5,
+    'JX' => 8,
+    'QZ' => 10,
+    ' \t\n' => 0
   }
 
-  @l_keys Map.keys(@l_values)
-
-
-  @doc """
-   Creating a new map of letter(key) to score(value) map.
-
-    seed_map = %{"A" => 1, "B" => 1 ..etc}
-
-  """
-  def seed_map do
-    Enum.reduce(@l_keys,%{}, fn x, acc -> list_to_map(x,acc) end)
-  end
-
-  defp list_to_map(key,acc) do
-    new_map = Map.new(@l_values[key], fn a -> {a,key} end )
-    Map.merge(acc,new_map)
-  end
-
+  @scores_map for {letters, value} <- @l_scores,
+                  letter <- letters,
+              into: %{}, do: {letter, value}
 
   @doc """
   Calculate the scrabble score for the word.
   """
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
-    if String.length(word) >= 1 do
-        if String.contains?(word," \t\n") do
-          0
-        else
-          map_l = seed_map()
           word
           |> String.upcase()
-          |> String.split("",trim: true)
-          |> Enum.reduce(0, fn x,acc -> acc + Map.fetch!(map_l,x) end)
-        end
-    else
-       0
-    end
+          |> String.to_charlist()
+          |> Enum.map(&Map.fetch!(@scores_map,&1))
+          |> Enum.sum
   end
+
+
 end
