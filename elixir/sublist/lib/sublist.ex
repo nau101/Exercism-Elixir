632760c79c0de9fb361b3ca3aacf59b849c95149
nau101 <rna0517@gmail.com>
2021-06-20 23:51:15 -0400

    Happy fathers day!  wip sublist

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -7,21 +7,23 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-    count_a = Enum.count(a)
-    count_b = Enum.count(b)
-
     cond do
-      a == b -> :equal
+      a === b -> :equal
       a == [] and b == [nil] -> :sublist
       a == [nil] and b == [] -> :superlist
-      count_a < count_b and check_list(a,b) -> :sublist
+      check_list(a,b) == :sublist -> :sublist
       a != b -> :unequal
     end
   end
 
   defp check_list(a,b) do
-    for x <- a , x === b, do: :sublist
-    |> Enum.filter(fn x )
+    count_a = Enum.count(a)
+    count_b = Enum.count(b)
+
+    if count_a < count_b do
+      res = for x <- a, Enum.member?(b,x), into: "", do: "sublist\s"
+      if String.contains?(res,"sublist") == true, do: :sublist
+    end
 
   end
 end
