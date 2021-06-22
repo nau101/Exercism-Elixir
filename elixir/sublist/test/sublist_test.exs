92e5dbbdc54bc6a91943368f26e9f89a9119faeb
nau101 <rna0517@gmail.com>
2021-06-21 23:58:34 -0400

    sublist wip

--- elixir/sublist/test/sublist_test.exs
+++ elixir/sublist/test/sublist_test.exs
@@ -31,7 +31,7 @@ defmodule SublistTest do
     assert Sublist.compare([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
   end
 
-  #@tag :pending
+ # @tag :pending
   test "sublist in middle" do
     assert Sublist.compare([4, 3, 2], [5, 4, 3, 2, 1]) == :sublist
   end
@@ -56,37 +56,37 @@ defmodule SublistTest do
     assert Sublist.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
   end
 
-  #@tag :pending
+  @tag :pending
   test "superlist at start" do
     assert Sublist.compare([1, 2, 3, 4, 5], [1, 2, 3]) == :superlist
   end
 
-  #@tag :pending
+  @tag :pending
   test "superlist in middle" do
     assert Sublist.compare([5, 4, 3, 2, 1], [4, 3, 2]) == :superlist
   end
 
-  #@tag :pending
+  @tag :pending
   test "superlist at end" do
     assert Sublist.compare([1, 2, 3, 4, 5], [3, 4, 5]) == :superlist
   end
 
-  #@tag :pending
+  @tag :pending
   test "1 and 2 does not contain 3" do
     assert Sublist.compare([1, 2], [3]) == :unequal
   end
 
-  #@tag :pending
+  @tag :pending
   test "partially matching superlist at start" do
     assert Sublist.compare([1, 1, 1, 2], [1, 1, 2]) == :superlist
   end
 
-  #@tag :pending
+  @tag :pending
   test "superlist early in huge list" do
     assert Sublist.compare(Enum.to_list(1..1_000_000), [3, 4, 5]) == :superlist
   end
 
-  #@tag :pending
+  @tag :pending
   test "strict equality needed" do
     assert Sublist.compare([1], [1.0, 2]) == :unequal
   end
