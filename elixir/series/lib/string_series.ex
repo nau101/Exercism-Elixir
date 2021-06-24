84fe5c99931401a0fa4f622ca9d377c1dbc47285
nau101 <rna0517@gmail.com>
2021-06-24 00:30:35 -0400

    Finished exercise string series

---
+++ elixir/series/lib/string_series.ex
@@ -0,0 +1,18 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+
+  def slices(_s,size) when size == 0 or size < 0, do: []
+  def slices(s, size) when size >= 1 do
+    s
+    |> String.graphemes()
+    |> Enum.chunk_every(size,1,:discard)
+    |> Enum.reduce([], fn x,acc -> acc ++ [Enum.join(x)]  end)
+
+  end
+
+end
